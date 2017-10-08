87b36c37d7b91b612847a734a9cd111a36045c73
Victor Ferreira <victtorferreiralima@gmail.com>
2017-10-08 15:33:14 -0300

    Create model items

--- core/models.py
+++ core/models.py
@@ -1,6 +1,30 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import datetime
 
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.conf import settings
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=140)
+    created_date = models.DateField(auto_now=True)
+    completed = models.BooleanField(default=None)
+    completed_date = models.DateField(blank=True, null=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='todo_created_by')
+    note = models.TextField(blank=True, null=True)
+    priority = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('todo-task_detail', kwargs={'task_id': self.id, })
+    
+    def save(self):
+        if self.completed:
+            self.completed_date = datetime.datetime.now()
+        super(Item, self).save()
+
+    class Meta:
+        ordering = ["priority"]
